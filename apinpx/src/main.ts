3b2e9fef17037f8b01c1d51c449b5d43184c43d3
Josep Berdugo <jberdugo88@gmail.com>
2026-02-26 11:24:37 -0500

    cambio en el cors para conectar back y front

--- apinpx/src/main.ts
+++ apinpx/src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   // 👉 Habilitar CORS para local y para producción
   app.enableCors({
     origin: [
-      'http://localhost:5173', 
-      'https://surcompany-tracking.netlify.app' // URL de tu frontend en Netlify
+      'http://localhost:5173',
+      'https://surcompanycotizaciones.pages.dev' // URL de tu frontend en cloudflare
     ],
     credentials: true,
   });
@@ -16,7 +16,7 @@ async function bootstrap() {
   // 👉 Escuchar en el puerto que asigne Render o el 3000 por defecto
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  
+
   console.log(`🚀 Backend running on port ${port}`);
 }
 bootstrap();
