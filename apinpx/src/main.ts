0a45a9e1a88d9b5a3dcf6b2d650317bb3fde873c
Josep Berdugo <jberdugo88@gmail.com>
2026-04-08 12:35:15 -0500

    CAMBIOS EN EL CORS

--- apinpx/src/main.ts
+++ apinpx/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
 
   // 👉 Escuchar en el puerto que asigne Render o el 3000 por defecto
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Backend running on port ${port}`);
 }
